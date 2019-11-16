581d5fde1d504b99d84be8612f1e11a501f19fe2
Geoff Bourne <itzgeoff@gmail.com>
2019-11-16 09:30:39 -0600

    ci: add adopt11 to docker-versions-create.sh

--- docker-versions-create.sh
+++ docker-versions-create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set -x
 # Use this variable to indicate a list of branches that docker hub is watching
-branches_list=('openj9' 'openj9-nightly')
+branches_list=('openj9' 'openj9-nightly' 'adopt11')
 
 function TrapExit {
   echo "Checking out back in master"
