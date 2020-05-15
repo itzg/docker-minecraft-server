fb74035a1bff228d4b399aa7922f19b3837f3730
Michael Kirsch <mkirsch.git@gmail.com>
2020-05-15 16:14:02 +0200

    compress pause script

--- files/autopause/pause.sh
+++ files/autopause/pause.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-if [[ $( ps -a -o stat,comm | grep 'java' | awk '{ print $1 }') =~ ^T.*$ ]] ; then
-  echo "[Autopause] Already stopped!" >/tmp/terminal-mc
-  STATUS=PA
-  export STATUS
-  exit 0
+if [[ $( ps -a -o stat,comm | grep 'java' | awk '{ print $1 }') =~ ^S.*$ ]] ; then
+  echo "[Autopause] Pausing Java process" >/tmp/terminal-mc
+  killall -q -STOP java
 fi
-echo "[Autopause] Pausing Java process" >/tmp/terminal-mc
-STATUS=PS
-export STATUS
-killall -q -STOP java
