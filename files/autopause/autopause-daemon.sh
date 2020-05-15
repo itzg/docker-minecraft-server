bafd8e3144a750e9017a328241cb5a321fb1ddae
Michael Kirsch <mkirsch.git@gmail.com>
2020-05-15 22:40:03 +0200

    fix function call and output states to the terminal

--- files/autopause/autopause-daemon.sh
+++ files/autopause/autopause-daemon.sh
@@ -7,49 +7,60 @@ exec 1>/tmp/terminal-mc
 sudo /usr/sbin/knockd -c /autopause/knockd-config.cfg -d
 
 STATE=K
-TIME_THRESH=current_uptime+$AUTOPAUSE_TIMEOUT_KN
+TIME_THRESH=$(($(current_uptime)+$AUTOPAUSE_TIMEOUT_KN))
 
 while :
 do
   case X$STATE in
   XK)
     # Knocked
-    if java_clients_connected; then
+    if java_clients_connected ; then
+      echo "[Autopause loop] Client connected - waiting for disconnect"
       STATE=E
     else
-      if [ current_uptime -ge $TIME_THRESH ]; then
-        exec /autopause/pause.sh
+      if [[ $(current_uptime) -ge $TIME_THRESH ]] ; then
+        echo "[Autopause loop] No client connected since startup - stopping"
+        /autopause/pause.sh
         STATE=S
       fi
     fi
     ;;
   XE)
     # Established
-    if ! java_clients_connected; then
-      TIME_THRESH=current_uptime+$AUTOPAUSE_TIMEOUT_EST
+    if ! java_clients_connected ; then
+      TIME_THRESH=$(($(current_uptime)+$AUTOPAUSE_TIMEOUT_EST))
+      echo "[Autopause loop] All clients disconnected - stopping in $AUTOPAUSE_TIMEOUT_EST seconds"
       STATE=I
     fi
     ;;
   XI)
     # Idle
-    if java_clients_connected; then
+    if java_clients_connected ; then
+      echo "[Autopause loop] Client reconnected - waiting for disconnect"
       STATE=E
     else
-      if [ current_uptime -ge $TIME_THRESH ]; then
-        exec /autopause/pause.sh
+      if [[ $(current_uptime) -ge $TIME_THRESH ]] ; then
+        echo "[Autopause loop] No client reconnected - stopping"
+        /autopause/pause.sh
         STATE=S
       fi
     fi
     ;;
   XS)
     # Stopped
-    if java_running; then
-      TIME_THRESH=current_uptime+$AUTOPAUSE_TIMEOUT_KN
+    if java_running ; then
+      if java_clients_connected ; then
+        echo "[Autopause loop] Client connected - waiting for disconnect"
+        STATE=E
+      else
+        TIME_THRESH=$(($(current_uptime)+$AUTOPAUSE_TIMEOUT_KN))
+        echo "[Autopause loop] Server was knocked - waiting for clients or timeout"
       STATE=K
+    fi
     fi
     ;;
   *)
-    echo "[Autopause loop]: Error: invalid state: $STATE"
+    echo "[Autopause loop] Error: invalid state: $STATE"
     ;;
   esac
   sleep $AUTOPAUSE_CHECK_INTERVAL
--- files/autopause/pause.sh
+++ files/autopause/pause.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ $( ps -a -o stat,comm | grep 'java' | awk '{ print $1 }') =~ ^S.*$ ]] ; then
-  echo "[Autopause] Pausing Java process" >/tmp/terminal-mc
+  echo "[$(date -Iseconds)] [Autopause] Pausing Java process" >/tmp/terminal-mc
   killall -q -STOP java
 fi
