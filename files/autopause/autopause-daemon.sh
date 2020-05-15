6a4807a8b95e30f5cf4e1062bc245293dda251ee
Michael Kirsch <mkirsch.git@gmail.com>
2020-05-15 16:15:22 +0200

    build state machine

--- files/autopause/autopause-daemon.sh
+++ files/autopause/autopause-daemon.sh
@@ -5,24 +5,49 @@ exec 1>/tmp/terminal-mc
 sudo /usr/sbin/knockd -c /autopause/knockd-config.cfg -d
 
 STATE=K
-TIME_THRESH=current_uptime+AUTOPAUSE_TIMEOUT_KN
+TIME_THRESH=current_uptime+$AUTOPAUSE_TIMEOUT_KN
 
 while :
 do
-  case "X$STATE" in
+  case X$STATE in
   XK)
     # Knocked
-
+    if java_clients_connected; then
+      STATE=E
+    else
+      if [ current_uptime -ge $TIME_THRESH ]; then
+        exec /autopause/pause.sh
+        STATE=S
+      fi
+    fi
     ;;
   XE)
     # Established
+    if ! java_clients_connected; then
+      TIME_THRESH=current_uptime+$AUTOPAUSE_TIMEOUT_EST
+      STATE=I
+    fi
     ;;
   XI)
     # Idle
+    if java_clients_connected; then
+      STATE=E
+    else
+      if [ current_uptime -ge $TIME_THRESH ]; then
+        exec /autopause/pause.sh
+        STATE=S
+      fi
+    fi
     ;;
   XS)
     # Stopped
-
+    if java_running; then
+      TIME_THRESH=current_uptime+$AUTOPAUSE_TIMEOUT_KN
+      STATE=K
+    fi
+    ;;
+  *)
+    echo "[Autopause loop]: Error: invalid state: $STATE"
     ;;
   esac
   sleep $AUTOPAUSE_CHECK_INTERVAL
@@ -32,7 +57,7 @@ current_uptime() {
   return awk '{print $1}' /proc/uptime
 }
 
-java_state() {
+java_running() {
   if [[ $( ps -a -o stat,comm | grep 'java' | awk '{ print $1 }') =~ ^S.*$ ]] ; then
     return 1
   else
