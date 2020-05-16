a550427be42dda6ac7c482760e0d4705a2cdbde2
Michael Kirsch <mkirsch.git@gmail.com>
2020-05-16 20:08:31 +0200

    exclude local connections as valid clients

--- files/autopause/autopause-fcns.sh
+++ files/autopause/autopause-fcns.sh
@@ -9,5 +9,22 @@ java_running() {
 }
 
 java_clients_connected() {
-  [[ -n "$(netstat -tn | grep $SERVER_PORT | grep ESTABLISHED)" ]]
+  local connections
+  connections=$(netstat -tn | grep ":$SERVER_PORT" | grep ESTABLISHED)
+  if [[ -z "$connections" ]] ; then
+    return 1
+  fi
+  IFS=$'\n'
+  connections=($connections)
+  unset IFS
+  # check that at least one external address is not localhost
+  # remember, that the host network mode does not work with autopause because of the knockd utility
+  for (( i=0; i<${#connections[@]}; i++ ))
+  do
+    if [[ ! $(echo "${connections[$i]}" | awk '{print $5}') =~ ^\s*127\.0\.0\.1:.*$ ]] ; then
+      # not localhost
+      return 0
+    fi
+  done
+  return 1
 }
