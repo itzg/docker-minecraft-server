9bf3d42252bf34e7edf640e4807b2d95717a73c4
Michael Kirsch <mkirsch.git@gmail.com>
2020-05-16 23:05:10 +0200

    save via rcon before suspending process

--- files/autopause/pause.sh
+++ files/autopause/pause.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [[ $( ps -a -o stat,comm | grep 'java' | awk '{ print $1 }') =~ ^S.*$ ]] ; then
+  rcon-cli save-all >/dev/null
   echo "[$(date -Iseconds)] [Autopause] Pausing Java process" >/tmp/terminal-mc
   killall -q -STOP java
 fi
