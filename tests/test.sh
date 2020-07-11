90183ae8236530b7c9cd6ef65bdeaa9cf532188b
Geoff Bourne <itzgeoff@gmail.com>
2020-07-11 14:26:47 -0500

    ci: Don't restart failed container during test

--- tests/test.sh
+++ tests/test.sh
@@ -5,6 +5,9 @@ cd $(dirname $0)
 failed=false
 args="-f docker-compose.test.yml"
 docker-compose $args run sut || failed=true
+echo "
+Result: failed=$failed"
+
 $failed && docker-compose $args logs mc
 docker-compose $args down -v
 
