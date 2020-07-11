367c6cfd92db74f26fc8ee7e410f7a68a9cdf1f1
Geoff Bourne <itzgeoff@gmail.com>
2020-07-11 14:09:08 -0500

    ci: Added testing step to github actions

---
+++ tests/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+failed=false
+args="-f docker-compose.test.yml"
+docker-compose $args run sut || failed=true; docker-compose $args logs mc
+docker-compose $args down -v
+
+if $failed; then
+  exit 1
+fi
+
