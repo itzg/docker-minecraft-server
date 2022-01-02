2f7a2cb49b6c45ec9f79b8a001329ba02a106ba5
christopher blodgett <christopher.blodgett@gmail.com>
2022-01-02 15:07:15 -0800

    adding recommended changes and using iteration to test docker-compose builds

--- tests/test.sh
+++ tests/test.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
+# go to script root directory
 cd "$(dirname "$0")" || exit 1
 
-failed=false
-
+# compose down function for reuse
 down() {
   docker-compose down -v
 }
 
-docker-compose run monitor || failed=true
-echo "
-Result: failed=$failed"
+# go through each folder to test builds
+FOLDERS=$(ls)
+for folder in $FOLDERS; do
+    # If folder is a directory
+    if [ -d "$folder" ]; then
+      cd "$folder"
+      failed=false
 
-if $failed; then
-  docker-compose logs mc
-  down
-  exit 1
-else
-  down
-fi
+      # run the monitor to validate the Minecraft image is healthy
+      docker-compose run monitor || failed=true
+      echo "${folder} Result: failed=$failed"
+
+      # TODO: findout more about logs mc
+      if $failed; then
+        docker-compose logs mc
+        down
+        cd ..
+        exit 2
+      else
+        down
+        cd ..
+      fi
+    fi
+done
+
+
+
+
 
