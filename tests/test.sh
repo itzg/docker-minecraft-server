c62e243d73aa0ee9d53037a3a775e3b6ad74eb80
Geoff Bourne <itzgeoff@gmail.com>
2022-01-23 18:29:08 -0600

    Fixed upper tests/test.sh recursion

--- tests/test.sh
+++ tests/test.sh
@@ -6,12 +6,10 @@ IFS=$'\n\t'
 cd "$(dirname "$0")" || exit 1
 
 # go through top level folders and trigger the tests in the subfolders
-readarray -t folders < <(find . -maxdepth 2 -mindepth 2 -name docker-compose.yml -printf '%h\n')
+readarray -t folders < <(find . -maxdepth 2 -mindepth 2 -name test.sh -printf '%h\n')
 for folder in "${folders[@]}"; do
   cd "$folder"
-  if [ -f "./test.sh" ]; then
-    echo "Starting ${folder} Tests"
-    sh ./test.sh
-  fi
+  echo "Starting ${folder} Tests"
+  sh ./test.sh
   cd ..
 done
