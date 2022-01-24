d49c933665bafada5233c824e967d7efa2728a28
Geoff Bourne <itzgeoff@gmail.com>
2022-01-23 18:23:56 -0600

    feat: support WORLD files in compressed tar format Fixes #1291

--- tests/setuponlytests/test.sh
+++ tests/setuponlytests/test.sh
@@ -1,43 +1,51 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 # go to script root directory
 cd "$(dirname "$0")" || exit 1
 
-# compose down function for reuse
-down() {
-  docker-compose down -v --remove-orphans
-}
-
-checkandExitOnFailure(){
-  failed=$1
-  # docker-compose logs outputs messages from the specified container
-  if $failed; then
-    docker-compose logs mc
-    down
-    cd ..
-    exit 2
-  fi
-}
-
-# tests that only run the setup files for things like downloads and configuration. 
+# tests that only run the setup files for things like downloads and configuration.
 setupOnlyMinecraftTest(){
   folder=$1
   cd "$folder"
-  failed=false
-  # run the monitor to validate the Minecraft image is healthy
-  docker-compose --log-level ERROR up --quiet-pull --exit-code-from mc 2>/dev/null || failed=true
-  echo "${folder} Result: failed=$failed"
-  checkandExitOnFailure $failed
-  down
+  result=0
+
+  if ! logs=$(docker compose run --quiet-pull mc 2>&1); then
+    echo "${folder} setup FAILED"
+    echo ":::::::::::: LOGS ::::::::::::::::
+$logs
+::::::::::::::::::::::::::::::::::
+"
+    result=1
+  elif [ -f verify.sh ]; then
+    if ! docker run --rm --entrypoint bash -v "${PWD}/data":/data -v "${PWD}/verify.sh":/verify "${IMAGE_TO_TEST:-itzg/minecraft-server}" /verify; then
+      echo "${folder} verify FAILED"
+      result=1
+    else
+      echo "${folder} verify PASS"
+    fi
+    rm -rf data
+  else
+    echo "${folder} PASS"
+  fi
+
+  docker compose down -v --remove-orphans
   cd ..
+
+  return $result
 }
 
 # go through each folder in setuponly and test setups
-FOLDERS=$(ls)
-for folder in $FOLDERS; do
-    # If folder is a directory
-    if [ -d "$folder" ]; then
-      echo "Starting Tests on ${folder}"
-      setupOnlyMinecraftTest $folder
-    fi
-done
+if (( $# > 0 )); then
+  for folder in "$@"; do
+    echo "Starting Tests in ${folder}"
+    setupOnlyMinecraftTest "$folder"
+  done
+else
+  readarray -t folders < <(find . -maxdepth 2 -mindepth 2 -name docker-compose.yml -printf '%h\n')
+  for folder in "${folders[@]}"; do
+    echo "Starting Tests in ${folder}"
+    setupOnlyMinecraftTest "$folder"
+  done
+fi
