54176a11b50456a421e42e22d31315fd0958c83a
christopher blodgett <christopher.blodgett@gmail.com>
2022-02-06 08:39:25 -0800

    removing logging that was used for testing, deduped download code

--- tests/setuponlytests/vanillatweaks_file/verify.sh
+++ tests/setuponlytests/vanillatweaks_file/verify.sh
@@ -1,4 +1,4 @@
 mc-image-helper assert fileExists "/data/world/datapacks/afk*"
 mc-image-helper assert fileExists "/data/world/datapacks/graves*"
-mc-image-helper assert fileExists /data/resourcepacks/resourcepacks.zip
-mc-image-helper assert fileExists /data/world/datapacks/crafting_tweaks.zip
+mc-image-helper assert fileExists "/data/world/datapacks/crafting_tweaks*"
+mc-image-helper assert fileExists "/data/resourcepacks/resourcepacks*"
