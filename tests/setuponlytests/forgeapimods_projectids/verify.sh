c19f91a1710b7c20516f4e210dc39184d15d015a
christopher blodgett <christopher.blodgett@gmail.com>
2022-02-24 12:38:40 -0800

    Fix for Random forge api failure, removing dependency duplication messages

--- tests/setuponlytests/forgeapimods_projectids/verify.sh
+++ tests/setuponlytests/forgeapimods_projectids/verify.sh
@@ -1,4 +1,5 @@
+# No family filter applied, DO NOT use Fabric or Forge specific name validation as it may cause random breakage.
 mc-image-helper assert fileExists "/data/mods/BiomesOPlenty*"
 mc-image-helper assert fileExists "/data/mods/TerraBlender*"
-mc-image-helper assert fileExists "/data/mods/voicechat-fabric*"
+mc-image-helper assert fileExists "/data/mods/voicechat*"
 mc-image-helper assert fileExists "/data/mods/fabric-api*"
