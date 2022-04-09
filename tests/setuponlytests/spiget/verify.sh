2d222b127801e7231aefe6d69a5239ea615f7969
Geoff Bourne <itzgeoff@gmail.com>
2022-04-09 17:27:56 -0500

    Correct spiget handling of jars that contain jars
    Fixes #1426

--- tests/setuponlytests/spiget/verify.sh
+++ tests/setuponlytests/spiget/verify.sh
@@ -1,4 +1,5 @@
 mc-image-helper assert fileExists plugins/3836.jar
 mc-image-helper assert fileExists plugins/34315.jar
 mc-image-helper assert fileExists plugins/6245.jar
+mc-image-helper assert fileExists plugins/88135.jar
 mc-image-helper assert fileExists plugins/SkinsRestorer.jar
