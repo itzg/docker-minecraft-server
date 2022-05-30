77ab9f21cd93923e57311c5b5d16a7c55f1d0c8b
Geoff Bourne <itzgeoff@gmail.com>
2022-05-30 12:53:51 -0500

    Updated verify

--- tests/setuponlytests/spiget/verify.sh
+++ tests/setuponlytests/spiget/verify.sh
@@ -1,5 +1,4 @@
 mc-image-helper assert fileExists plugins/3836.jar
 mc-image-helper assert fileExists plugins/34315.jar
 mc-image-helper assert fileExists plugins/6245.jar
-mc-image-helper assert fileExists plugins/88135.jar
 mc-image-helper assert fileExists plugins/SkinsRestorer.jar
