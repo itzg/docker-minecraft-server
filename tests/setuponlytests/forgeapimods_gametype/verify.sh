4f9de809f81122307f72780fe450738e4f481ca7
chblodg <shotah@users.noreply.github.com>
2022-06-05 13:16:12 -0700

    updating forgeapi tests and adding is false (#1548)
    Co-authored-by: christopher blodgett <christopher.blodgett@gmail.com>

--- tests/setuponlytests/forgeapimods_gametype/verify.sh
+++ tests/setuponlytests/forgeapimods_gametype/verify.sh
@@ -1,5 +1,5 @@
-mc-image-helper assert fileExists "/data/mods/BiomesOPlenty*"
 # testing dependencies don't get downloaded when download dependencies is set to false.
-! mc-image-helper assert fileExists "/data/mods/TerraBlender*"
 mc-image-helper assert fileExists "/data/mods/voicechat-fabric*"
-mc-image-helper assert fileExists "/data/mods/fabric-api*"
+mc-image-helper assert fileExists "/data/mods/flan*"
+# Dependent of flan, but dependencies are set to false:
+! mc-image-helper assert fileExists "/data/mods/fabric-api*"
