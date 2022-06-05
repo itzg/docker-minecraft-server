e7723e6b92616d629bbc44cd181be9dad8c465ae
christopher blodgett <christopher.blodgett@gmail.com>
2022-06-05 10:20:44 -0700

    updating forgeapi tests and adding is false

--- tests/setuponlytests/forgeapimods_file/verify.sh
+++ tests/setuponlytests/forgeapimods_file/verify.sh
@@ -1,5 +1,5 @@
 # Validates specific beta call out for specific mod:
 mc-image-helper assert fileExists "/data/mods/voicechat-fabric*"
-mc-image-helper assert fileExists "/data/mods/onastick-fabric*"
-# Dependent of on a stick:
+mc-image-helper assert fileExists "/data/mods/flan*"
+# Dependent of flan:
 mc-image-helper assert fileExists "/data/mods/fabric-api*"
