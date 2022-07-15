ef43e948f93205ad0064469afdeab726efd73cdc
NoaHimesaka1873 <himesaka@noa.codes>
2022-07-15 10:20:44 +0900

    Working image

---
+++ build/ol/install-packages.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+microdnf install dnf -y
+
+dnf install 'dnf-command(config-manager)' -y
+dnf config-manager --set-enabled ol8_codeready_builder
+tee /etc/yum.repos.d/ol8-epel.repo<<EOF
+[ol8_developer_EPEL]
+name= Oracle Linux \$releasever EPEL (\$basearch)
+baseurl=https://yum.oracle.com/repo/OracleLinux/OL8/developer/EPEL/\$basearch/
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-oracle
+gpgcheck=1
+enabled=1
+EOF
+dnf update -y
+
+dnf install -y ImageMagick \
+  file \
+  sudo \
+  net-tools \
+  iputils \
+  curl \
+  git \
+  jq \
+  dos2unix \
+  mysql \
+  tzdata \
+  rsync \
+  nano \
+  unzip \
+  zstd \
+  lbzip2 \
+  knock
+
+bash /build/ol/install-gosu.sh
