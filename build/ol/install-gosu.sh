df203d4b4ba7b8fd6c412633489ceec2c0077445
Noa Himesaka <himesaka@noa.codes>
2022-07-18 00:06:33 +0900

    Fix GraalVM images error on AArch64 systems (#1609)

--- build/ol/install-gosu.sh
+++ build/ol/install-gosu.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-[[ $(uname -m) == "aarch64" ]] && curl -sL -o /bin/gosu https://github.com/tianon/gosu/releases/download/1.14/gosu-arm64 && chmod +x /bin/gosu
-[[ $(uname -m) == "x86_64" ]] && curl -sL -o /bin/gosu https://github.com/tianon/gosu/releases/download/1.14/gosu-amd64 && chmod +x /bin/gosu
-
+if [[ $(uname -m) == "aarch64" ]]; then 
+    curl -sL -o /bin/gosu https://github.com/tianon/gosu/releases/download/1.14/gosu-arm64
+    chmod +x /bin/gosu
+elif [[ $(uname -m) == "x86_64" ]]; then 
+    curl -sL -o /bin/gosu https://github.com/tianon/gosu/releases/download/1.14/gosu-amd64
+    chmod +x /bin/gosu
+else
+    echo "Not supported!"
+    exit 1
+fi  
