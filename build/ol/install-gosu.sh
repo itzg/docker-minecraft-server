6cda76ac1a932b69bdabcf1aad64ff57b591f437
Noa Himesaka <himesaka@noa.codes>
2022-07-17 23:09:45 +0900

    use if-else

--- build/ol/install-gosu.sh
+++ build/ol/install-gosu.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-[[ $(uname -m) == "aarch64" ]] && curl -sL -o /bin/gosu https://github.com/tianon/gosu/releases/download/1.14/gosu-arm64 && chmod +x /bin/gosu || echo "not aarch64"
-[[ $(uname -m) == "x86_64" ]] && curl -sL -o /bin/gosu https://github.com/tianon/gosu/releases/download/1.14/gosu-amd64 && chmod +x /bin/gosu || echo "not x86_64"
+if [[ $(uname -m) == "aarch64" ]]; then 
+    curl -sL -o /bin/gosu https://github.com/tianon/gosu/releases/download/1.14/gosu-arm64
+    chmod +x /bin/gosu
+elif [[ $(uname -m) == "x86_64" ]]; then 
+    curl -sL -o /bin/gosu https://github.com/tianon/gosu/releases/download/1.14/gosu-amd64
+    chmod +x /bin/gosu
+else
+    echo "Not supported!"
+fi  
