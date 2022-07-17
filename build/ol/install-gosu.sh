2297378a070dbf8c81cbade85d405b2188e21122
Noa Himesaka <himesaka@noa.codes>
2022-07-17 11:47:43 +0900

    Fix GraalVM images error on AArch64 systems
    This one trick makes it build on AArch64 systems, tested on Oracle Cloud A1.Flex instance.

--- build/ol/install-gosu.sh
+++ build/ol/install-gosu.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-[[ $(uname -m) == "aarch64" ]] && curl -sL -o /bin/gosu https://github.com/tianon/gosu/releases/download/1.14/gosu-arm64 && chmod +x /bin/gosu
-[[ $(uname -m) == "x86_64" ]] && curl -sL -o /bin/gosu https://github.com/tianon/gosu/releases/download/1.14/gosu-amd64 && chmod +x /bin/gosu
-
+[[ $(uname -m) == "aarch64" ]] && curl -sL -o /bin/gosu https://github.com/tianon/gosu/releases/download/1.14/gosu-arm64 && chmod +x /bin/gosu || echo "not aarch64"
+[[ $(uname -m) == "x86_64" ]] && curl -sL -o /bin/gosu https://github.com/tianon/gosu/releases/download/1.14/gosu-amd64 && chmod +x /bin/gosu || echo "not x86_64"
