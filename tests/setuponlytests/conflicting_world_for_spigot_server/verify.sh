fabe14db4985a5c8829f85ff9241b6532cf4b972
Geoff Bourne <itzgeoff@gmail.com>
2022-09-10 12:58:32 -0500

    Download and use packwiz from Maven repository (#1725)
    Also
    * Added github actions debug support
    * added use of fileNotExists

--- tests/setuponlytests/conflicting_world_for_spigot_server/verify.sh
+++ tests/setuponlytests/conflicting_world_for_spigot_server/verify.sh
@@ -1,5 +1,7 @@
-mc-image-helper assert fileExists world/level.dat && \
-mc-image-helper assert fileExists world_nether/DIM-1/some_spigot_nether_file && \
-mc-image-helper assert fileExists world_the_end/DIM1/some_spigot_end_file && \
-! mc-image-helper assert fileExists world_nether/DIM-1/some_vanilla_nether_file && \
-! mc-image-helper assert fileExists world_the_end/DIM1/some_vanilla_end_file
+mc-image-helper assert fileExists \
+  world/level.dat \
+  world_nether/DIM-1/some_spigot_nether_file \
+  world_the_end/DIM1/some_spigot_end_file
+mc-image-helper assert fileNotExists \
+  world_nether/DIM-1/some_vanilla_nether_file \
+  world_the_end/DIM1/some_vanilla_end_file
