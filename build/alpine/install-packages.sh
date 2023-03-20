7aec0bc54a52190b3dcc419f6d9aae95a6161ad7
Metalcape <dario.capecchi.m@gmail.com>
2023-03-20 18:47:53 +0100

    Install patched knockd in `install-packages.sh` Use a binary built with musl for alpine

--- build/alpine/install-packages.sh
+++ build/alpine/install-packages.sh
@@ -25,3 +25,8 @@ apk add --no-cache -U \
     zstd \
     nfs-utils \
     libpcap
+
+# Patched knockd
+cd /tmp && wget https://github.com/Metalcape/knock/releases/download/0.8.1/knock-0.8.1-alpine-amd64.tar.gz
+tar -xf /tmp/knock-0.8.1-alpine-amd64.tar.gz -C /usr/local/
+ln -s /usr/local/sbin/knockd /usr/sbin/knockd
