31e59f3b3cbfb308737b81506d76219123dd8de8
Ryan Conrad <camalot@gmail.com>
2023-04-04 14:33:31 -0500

    pass the value to assert in quotes

--- tests/setuponlytests/defaults/verify.sh
+++ tests/setuponlytests/defaults/verify.sh
@@ -1,4 +1,4 @@
 # EXPECT_RCON_PASSWORD is passed in to the test environment from the action generated output
-mc-image-helper assert propertyEquals --file=server.properties --property=rcon.password --expect=$EXPECT_RCON_PASSWORD
+mc-image-helper assert propertyEquals --file=server.properties --property=rcon.password --expect="${EXPECT_RCON_PASSWORD}"
 mc-image-helper assert propertyEquals --file=server.properties --property=rcon.port --expect=25575
 mc-image-helper assert propertyEquals --file=server.properties --property=enable-rcon --expect=true
