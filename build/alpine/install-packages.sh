94bb41ec35daf569afd276663e29346fb48d828f
Geoff Bourne <itzgeoff@gmail.com>
2024-02-07 17:16:53 -0600

    build: use libcap package for Alpine images

--- build/alpine/install-packages.sh
+++ build/alpine/install-packages.sh
@@ -27,7 +27,7 @@ apk add --no-cache -U \
     nfs-utils \
     libpcap \
     libwebp \
-    libcap-utils
+    libcap
 
 # Patched knockd
 curl -fsSL -o /tmp/knock.tar.gz https://github.com/Metalcape/knock/releases/download/0.8.1/knock-0.8.1-alpine-amd64.tar.gz
