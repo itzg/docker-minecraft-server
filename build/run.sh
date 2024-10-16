2a16fe5abe5330d0fe5bd0a68cbbaaec74b6740a
Tristan Perrin <ironminegg@gmail.com>
2024-10-16 02:19:49 +0200

    refacto git credentials

--- build/run.sh
+++ build/run.sh
@@ -5,3 +5,12 @@ set -e
 distro=$(cat /etc/os-release | grep -E "^ID=" | cut -d= -f2 | sed -e 's/"//g')
 
 "$(dirname "$0")/${distro}/$1".sh
+
+if [ "$1" = "install-packages" ]; then
+# Set Git credentials globally
+    cat <<EOF >> /etc/gitconfig
+[user]
+	name = Minecraft Server on Docker
+	email = server@example.com
+EOF
+fi
