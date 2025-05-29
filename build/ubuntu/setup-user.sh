257646d1e501594bb3d920b74cb1573c9b6ce990
1xenonn <88381044+peri123214@users.noreply.github.com>
2025-05-29 20:03:39 +0200

    Adding a separate user for Knockd (Ubuntu systems)

--- build/ubuntu/setup-user.sh
+++ build/ubuntu/setup-user.sh
@@ -7,4 +7,6 @@ if id ubuntu > /dev/null 2>&1; then
 fi
 
 addgroup --gid 1000 minecraft
+addgroup --gid 1001 knockd
 adduser --system --shell /bin/false --uid 1000 --ingroup minecraft --home /data minecraft
+adduser --system --shell /bin/false --uid 1001 --ingroup knockd knockd
