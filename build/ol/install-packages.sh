2e1738160d52afbc5d8a6354ae4b7a59ca1a486c
kingcavespider1 <brendan.oconnell@mail.ru>
2025-06-04 11:52:30 -0400

    cleanup ol build change

--- build/ol/install-packages.sh
+++ build/ol/install-packages.sh
@@ -4,20 +4,18 @@ export TARGET
 
 set -euo pipefail
 
-VERSION_ID=$(awk -F= '$1=="VERSION_ID" { print $2 ;}' /etc/os-release)//\"/%%.*
-VERSION_ID=${VERSION_ID//\"/}
-VERSION_ID=${VERSION_ID%%.*}
+os_major_version=$(awk -F'[= ."]+' '/^VERSION_ID=/{ print $2 }' /etc/os-release)
 
 # Install and configure dnf
 microdnf install dnf -y
 dnf install 'dnf-command(config-manager)' -y
-dnf config-manager --set-enabled ol${VERSION_ID}_codeready_builder
+dnf config-manager --set-enabled ol${os_major_version}_codeready_builder
 
 # Add EPEL repository
-tee /etc/yum.repos.d/ol${VERSION_ID}-epel.repo <<EOF
-[ol${VERSION_ID}_developer_EPEL]
+tee /etc/yum.repos.d/ol${os_major_version}-epel.repo <<EOF
+[ol${os_major_version}_developer_EPEL]
 name=Oracle Linux \$releasever EPEL (\$basearch)
-baseurl=https://yum.oracle.com/repo/OracleLinux/OL${VERSION_ID}/developer/EPEL/\$basearch/
+baseurl=https://yum.oracle.com/repo/OracleLinux/OL${os_major_version}/developer/EPEL/\$basearch/
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-oracle
 gpgcheck=1
 enabled=1
