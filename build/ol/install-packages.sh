ec4a46ed634569b7a32936ab273ddf9cacb8d778
Geoff Bourne <itzgeoff@gmail.com>
2026-01-26 18:09:32 -0600

    Investigate graalvm image variant build failures

--- build/ol/install-packages.sh
+++ build/ol/install-packages.sh
@@ -66,6 +66,9 @@ dnf clean all
 
 # Download and install patched knockd
 curl -fsSL -o /tmp/knock.tar.gz https://github.com/Metalcape/knock/releases/download/0.8.1/knock-0.8.1-$TARGET.tar.gz
+# DEBUG
+ls -ld /usr/local
+ls -l /usr/local
 tar -xf /tmp/knock.tar.gz -C /usr/local/ && rm /tmp/knock.tar.gz
 ln -s /usr/local/sbin/knockd /usr/sbin/knockd
 ls -l /usr/local/sbin/knockd
